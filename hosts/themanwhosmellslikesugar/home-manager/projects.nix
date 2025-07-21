deaa10fe5aec07cd4054881221a300df189c5885
Dmitriy Ivanko <tmwsls12@gmail.com>
2025-07-21 17:10:30 +0300

    Refactor installing projects

--- hosts/themanwhosmellslikesugar/home-manager/projects.nix
+++ hosts/themanwhosmellslikesugar/home-manager/projects.nix
@@ -7,36 +7,74 @@
 let
   projectsDir = "${config.home.homeDirectory}/Projects";
   documentsDir = "${config.home.homeDirectory}/Documents";
+
+  korallmicroProjects = [
+      "dracula"
+  ];
+
+  cusdebProjects = [
+    "hammett"
+  ];
+
+  tntProjects = [
+    "tutorin.tech"
+  ];
+
+  radiusProjects = [
+    "backend/platform-boards"
+    "backend/platform-survey"
+    "backend/platform-middleware"
+    "backend/platform-authorization"
+    "backend/platform-realtime-ms"
+    "backend/platform-matrix"
+    "app-chart"
+  ];
+
+  infraRadiusProjects = [
+    "pipeline"
+  ];
+
+  genGitCloneCmd = repoInfo: ''
+    if [ ! -d "${repoInfo.dest}" ]; then
+      ${pkgs.git}/bin/git clone ${repoInfo.url} ${repoInfo.dest}
+    fi
+  '';
+
+  repos =
+    (map (name: {
+      dest = "${projectsDir}/korallmicro/${name}";
+      url = "git@github.com:CusDeb-Solutions/${name}.git";
+    }) korallmicroProjects) ++
+
+    (map (name: {
+      dest = "${projectsDir}/cusdeb/${name}";
+      url = "git@github.com:cusdeb-com/${name}.git";
+    }) cusdebProjects) ++
+
+    (map (name: {
+      dest = "${projectsDir}/cusdeb/${name}";
+      url = "git@github.com:tutorin-tech/${name}.git";
+    }) tntProjects) ++
+
+    (map (name: {
+      dest = "${projectsDir}/radius/${lib.last (lib.splitString "/" name)}";
+      url = "git@gitlab-yc.myradius.ru:myradius/${name}.git";
+    }) radiusProjects) ++
+
+    (map (name: {
+      dest = "${projectsDir}/radius/${name}";
+      url = "git@gitlab-yc.myradius.ru:infra/${name}.git";
+    }) infraRadiusProjects);
 in
 {
-  home.activation.cloneRepos = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p ${projectsDir}
+  home.activation.cloneRepos = lib.hm.dag.entryAfter [ "writeBoundary" ] (''
+    mkdir -p ${projectsDir} ${documentsDir}
     export PATH=${pkgs.openssh}/bin:${pkgs.git}/bin:$PATH
-
-    if [ ! -d "${projectsDir}/radius/platform-boards" ]; then
-      ${pkgs.git}/bin/git clone git@gitlab-yc.myradius.ru:myradius/backend/platform-boards.git ${projectsDir}/radius/platform-boards
-    fi
-
-    if [ ! -d "${projectsDir}/radius/platform-survey" ]; then
-      ${pkgs.git}/bin/git clone git@gitlab-yc.myradius.ru:myradius/backend/platform-survey.git ${projectsDir}/radius/platform-survey
-    fi
-
-    if [ ! -d "${projectsDir}/radius/platform-middleware" ]; then
-      ${pkgs.git}/bin/git clone git@gitlab-yc.myradius.ru:myradius/backend/platform-middleware.git ${projectsDir}/radius/platform-middleware
-    fi
-
-    if [ ! -d "${projectsDir}/radius/app-chart" ]; then
-      ${pkgs.git}/bin/git clone git@gitlab-yc.myradius.ru:myradius/app-chart.git ${projectsDir}/radius/app-chart
-    fi
-
-    if [ ! -d "${projectsDir}/cusdeb/hammett" ]; then
-      ${pkgs.git}/bin/git clone git@github.com:cusdeb-com/hammett.git ${projectsDir}/cusdeb/hammett
-    fi
 
     if [ ! -d "${documentsDir}/mind-db" ]; then
       ${pkgs.git}/bin/git clone git@github.com:Themanwhosmellslikesugar/mind-db.git ${documentsDir}/mind-db
     fi
-  '';
+  '' + lib.concatStringsSep "\n" (map genGitCloneCmd repos));
 
   home.file."Projects/radius/platform-boards/.zed/settings.json".text = ''
     {
