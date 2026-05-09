425fcc0d35274345c8735e14c27ab013fb46fe2a
Dmitrii Ivanko <tmwsls12@gmail.com>
2026-05-09 17:31:40 +0300

    Use new config file path for firefox

--- hosts/themanwhosmellslikesugar/home-manager/firefox.nix
+++ hosts/themanwhosmellslikesugar/home-manager/firefox.nix
@@ -1,10 +1,12 @@
 {
+  config,
   lib,
   ...
 }:
 {
   programs.firefox = {
     enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
 
     profiles.themanwhosmellslikesugar = {
       search = {
